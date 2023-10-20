58597ed9e8beb36a338592ac89f426aa0f1fcf3e
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-10-19 17:59:59 -0700

    compressor.sh

--- compressor.sh
+++ compressor.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-copy_honeys(){
+packer(){
 
-subdirectorios=("/data/adbhoney" "/data/cowrie" "/data/dionaea")
-directorio_destino="/home/tsec/pruebas"
+honeys=("/data/adbhoney" "/data/cowrie" "/data/dionaea")
+catches="/home/tsec/catches"
+temp="/home/tsec"
 
-for subdir in "${subdirectorios[@]}"; do
-    cp -r "$subdir" "$directorio_destino"
+for subdirs in "${honeys[@]}"; do
+    cp -r "$subdirs" "$catches"
 done
 
+tar -cvf $temp/catches.tar $catches
+tar -czvf $temp/$(date +%A)_catches.tar.gz $temp/catches.tar && rm $temp/catches.tar
+sleep 5                
+
+scp $temp/$(date +%A)_catches.tar.gz root@192.17.200.101:/home/captures
+sleep 10
+rm $temp/$(date +%A)_catches.tar.gz && rm -r $catches/*
+
 }
-
-
-tar -cvf 
-tar -czvf                       #agregar gz
-
-scp /home/tsec/PCAP/tcpdump.pcap root@192.17.200.101:/home/
+packer
