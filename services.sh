fbed25cd43c080443f5eae528b0057f5aa603932
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-11-21 13:00:55 -0800

    Update services.sh

--- services.sh
+++ services.sh
@@ -3,9 +3,7 @@ set_services(){
     
     local sys="/etc/systemd/system/"
     mv tcpdump.service $sys
-    mv tcpdump.timer $sys
     systemctl daemon-reload 
-    systemctl enable tcpdump.timer
     systemctl enable tcpdump.service
 
 }
